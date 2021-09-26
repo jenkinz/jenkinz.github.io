8694cba73147563aaedfc216b21f26d0ff971fce
Brian Jenkins <bj@jenkinz.com>
2021-09-26 08:45:18 -0700

    Improve PDF top margins and breaks

--- jsonresume-theme-bjenkins/index.js
+++ jsonresume-theme-bjenkins/index.js
@@ -7,9 +7,11 @@ module.exports = {
 
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var printcss = fs.readFileSync(__dirname + "/print.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
 	return Handlebars.compile(template)({
 		css: css,
+		printcss: printcss,
 		resume: resume
 	});
 }
