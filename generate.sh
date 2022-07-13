e6b930b8b77c9a3e5f8ea6d64a7cf5486ace5097
Brian Jenkins <brian@jenkinz.com>
2022-07-13 16:37:28 -0700

    Add comment to script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Generate
+
 resume export index.html --format html --theme jsonresume-theme-bjenkins
 resume export brianjenkins_resume.pdf --format pdf --theme ./jsonresume-theme-bjenkins/
 
